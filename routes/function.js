a38abc8218fe29e4a0d7dfea81b7de94f3fec989
sunilpawar8390 <74348181+sunilpawar8390@users.noreply.github.com>
2022-03-23 17:55:33 +0530

    sepratly search api book, category, publisher

--- routes/function.js
+++ routes/function.js
@@ -3,12 +3,11 @@ const categorydetails = require("../models/Categorydetailslkp");
 const publsisherdetails = require("../models/Publisherdetailslkp");
 
 module.exports = (app) => {
-
-  //All Book Record GET API 
+  //All Book Record GET API
 
   app.get("/api/BooksDetails", (req, res) => {
     bookdetails
-      .find({IsActive: 'true'})
+      .find({ IsActive: "true" })
       .populate("Category")
       .populate("Publisher")
       .exec((err, bookdetails) => {
@@ -16,103 +15,146 @@ module.exports = (app) => {
       });
   });
 
-  //get Book Data by ID API 
+  //get Book Data by ID API
 
-  app.get('/api/BooksDetails/:bookId', (req, res) => {
-    bookdetails.findOne({ BDID: req.params.bookId })
-    .populate('Category')
-    .populate('Publisher')
-    .exec((err, result) => {
+  app.get("/api/BooksDetails/:bookId", (req, res) => {
+    bookdetails
+      .findOne({ BDID: req.params.bookId })
+      .populate("Category")
+      .populate("Publisher")
+      .exec((err, result) => {
         return res.send(result);
-    });
-  });
-
-  //Edit Book API 
-
-  app.post('/api/BooksDetails/edit', (req, res)=>{
-    categorydetails.findOne({Name: req.body.data.Category.Name}, (err, found1) => {
-      publsisherdetails.findOne({Name: req.body.data.Publisher.Name}, (err, found2) => {
-        bookdetails.findOne({ BDID: req.body.id }, (err, found3) => {
-          if(found3) {
-            found3.Bookname = req.body.data.Bookname;
-            found3.Category = found1._id;
-            found3.Publisher = found2._id;
-            found3.quantity = req.body.data.quantity;
-            return found3.save((err) => {
-              return res.send({status: 'Edit Book Succesfully'});
-            })
-          }
-        });
       });
-    });
   });
 
-  //Add New Book API 
+  //Edit Book API
 
-  app.post('/api/BooksDetails/add', (req, res)=>{
-    categorydetails.findOne({Name: req.body.data.Category.Name}, (err, found1) => {
-      publsisherdetails.findOne({Name: req.body.data.Publisher.Name}, (err, found2) => {
-        bookdetails.findOne({ BDID: req.body.data.BDID }, (err, found3) => {
-          if(!found3) {
-            const newData = new bookdetails();
-            newData.BDID = req.body.data.BDID;
-            newData.Bookname = req.body.data.Bookname;
-            newData.Category = found1._id;
-            newData.Publisher = found2._id;
-            newData.quantity = req.body.data.quantity;
-            return newData.save((err) => { 
-              return res.send({status: 'New Book add Succesfully' })
-                
-               
-            })
+  app.post("/api/BooksDetails/edit", (req, res) => {
+    categorydetails.findOne(
+      { Name: req.body.data.Category.Name },
+      (err, found1) => {
+        publsisherdetails.findOne(
+          { Name: req.body.data.Publisher.Name },
+          (err, found2) => {
+            bookdetails.findOne({ BDID: req.body.id }, (err, found3) => {
+              if (found3) {
+                found3.Bookname = req.body.data.Bookname;
+                found3.Category = found1._id;
+                found3.Publisher = found2._id;
+                found3.quantity = req.body.data.quantity;
+                return found3.save((err) => {
+                  return res.send({ status: "Edit Book Succesfully" });
+                });
+              }
+            });
           }
-          else
-          { 
-            return res.send({exist: `the Id is Already Exist ${ req.body.data.BDID }` })
-          }
-        });
-      });
-    });
+        );
+      }
+    );
   });
 
-  //Delete Book API 
+  //Add New Book API
+
+  app.post("/api/BooksDetails/add", (req, res) => {
+    categorydetails.findOne(
+      { Name: req.body.data.Category.Name },
+      (err, found1) => {
+        publsisherdetails.findOne(
+          { Name: req.body.data.Publisher.Name },
+          (err, found2) => {
+            bookdetails.findOne({ BDID: req.body.data.BDID }, (err, found3) => {
+              if (!found3) {
+                const newData = new bookdetails();
+                newData.BDID = req.body.data.BDID;
+                newData.Bookname = req.body.data.Bookname;
+                newData.Category = found1._id;
+                newData.Publisher = found2._id;
+                newData.quantity = req.body.data.quantity;
+                return newData.save((err) => {
+                  return res.send({ status: "New Book add Succesfully" });
+                });
+              } else {
+                return res.send({
+                  exist: `the Id is Already Exist ${req.body.data.BDID}`,
+                });
+              }
+            });
+          }
+        );
+      }
+    );
+  });
+
+  //Delete Book API
 
   app.get("/api/BooksDetails/delete/:BookId", (req, res) => {
-    bookdetails.findOne({BDID:  req.params.BookId}, (err, foundBookdetails) => {
-        if(foundBookdetails) {
-            foundBookdetails.IsActive = !foundBookdetails.IsActive;
+    bookdetails.findOne(
+      { BDID: req.params.BookId },
+      (err, foundBookdetails) => {
+        if (foundBookdetails) {
+          foundBookdetails.IsActive = !foundBookdetails.IsActive;
 
-            foundBookdetails.save((err) => {
-                res.send({ status: 'OK', message: 'Deleted succesfully', bookd:foundBookdetails });
-            })
+          foundBookdetails.save((err) => {
+            res.send({
+              status: "OK",
+              message: "Deleted succesfully",
+              bookd: foundBookdetails,
+            });
+          });
         }
-    });
+      }
+    );
   });
 
-
-  //Search Book API 
+  //Search Book API
   app.get("/api/BooksDetails/Search/:inputData", (req, res) => {
-    bookdetails.find({ Bookname : {"$regex": req.params.inputData,  "$options": "i"} })
-    .populate('Category')
-    .populate('Publisher')
-    .exec((err, result) => {
-        console.log(result);
+    bookdetails
+      .find({ Bookname: { $regex: req.params.inputData, $options: "i" } })
+      .populate("Category")
+      .populate("Publisher")
+      .exec((err, result) => {
+        //console.log(result);
         return res.send(result);
-    });
+      });
   });
 
-  //Search Category API 
-  // app.get("/api/BooksDetails/Search1/:inputDataCategory", (req, res) => {
-  //  bookdetails.find( { "Category.Name": { $regex: req.params.inputDataCategory } }) 
-   
-  //   .exec((err, result) => {
-  //       console.log(result);
-  //       return res.send(result);
-  //  // console.log(req.params.inputDataCategory);
-  //   });
-  // }); 
+  //Search Category API
+  app.get("/api/BooksDetails/Search1/:inputDataCategory", (req, res) => {
+    categorydetails
+      .find({ Name: { $regex: req.params.inputDataCategory } })
+      .exec((err, result, ) => {
+     
+         const data = result.map((bk) => {
+           bookdetails.find({Category: bk._id})
+           .populate("Category")
+           .populate("Publisher")
+           .exec((err, result1)=>{
+             console.log(result1);
+             return res.send(result1);
+           })
+        }); 
+      });
+  });
 
-//GET Catagory Details API 
+  //Search Publisher API
+  app.get("/api/BooksDetails/Search2/:inputDataPublisher", (req, res) => {
+    publsisherdetails
+      .find({ Name: { $regex: req.params.inputDataPublisher } })
+      .exec((err, result, ) => {
+     
+         const data = result.map((bk) => {
+           bookdetails.find({Publisher: bk._id})
+           .populate("Category")
+           .populate("Publisher")
+           .exec((err, result1)=>{
+             console.log(result1);
+             return res.send(result1);
+           })
+        }); 
+      });
+  });
+
+  //GET Catagory Details API
 
   app.get("/api/categorydetails", (req, res) => {
     categorydetails.find().exec((err, categorydetails) => {
@@ -120,7 +162,7 @@ module.exports = (app) => {
     });
   });
 
-  //Get Publsher Deatisl API 
+  //Get Publsher Deatisl API
 
   app.get("/api/publisherdetails", (req, res) => {
     publsisherdetails.find().exec((err, publsisherdetails) => {
